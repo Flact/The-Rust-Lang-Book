ac8046f36ca54d55124b0bc7c0eb611f23104850
Anushka Milinda <y5heCH@pm.me>
2022-10-30 02:13:41 -0400

    Getting user inputs

--- a_number_guessing_game/src/main.rs
+++ a_number_guessing_game/src/main.rs
@@ -1,3 +1,26 @@
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-----------Guess the Number-----------");
+    println!("Please input your guess:");
+
+    let rand_number = rand::thread_rng().gen_range(1..=100);
+    println!("The Secret Number Is: {}",rand_number);
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read the line");
+
+    
+
+    println!("Your guess is: {}", guess);
+
+    match guess.cmp(&rand_number){
+        Ordering::Equal => println!("To large"),
+        Ordering::Greater => println!("To big"),
+        Ordering::Less => println!("To small"),
+    }
 }
